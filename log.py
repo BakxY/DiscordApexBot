15d238be5fe552168a8de6af721cf08f41aa993e
Alessandro Hofmann <76223353+FischerTG@users.noreply.github.com>
2022-02-28 21:07:37 +0100

    updated log.py
    add comments to functions

--- log.py
+++ log.py
@@ -2,11 +2,13 @@
 import datetime
 import shutil
 
+# create log file
 def LogFile_Create():
     LATEST_LOG_FILE = open('src/log/latest.log', 'w')
     LATEST_LOG_FILE.write(str(datetime.date.today()) + '\n')
     LATEST_LOG_FILE.close()
 
+# check if current log file is outdated and store outdated log file
 def LogFile_CheckDate():
     LATEST_LOG_FILE = open('src/log/latest.log', 'r')
     Date = LATEST_LOG_FILE.readline()
@@ -16,6 +18,7 @@ def LogFile_CheckDate():
         shutil.copy('src/log/latest.log', 'src/log/' + Date + '.log')
         LogFile_Create()
 
+# write to log file in append mode and with timestamp
 def LogFile_WriteLog(Error):
     LATEST_LOG_FILE = open('src/log/latest.log', 'a')
     LATEST_LOG_FILE.write('[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ']: ' + Error + '\n')
